804f072c120d5d82e26a23eb06cb81d650d5c167
JesusBorrero <shyfu98@gmail.com>
2020-04-22 11:02:40 +0200

    First Commit

---
+++ Practica2SD/src/main/java/com/practica2sd/Practica2SdApplication.java
@@ -0,0 +1,13 @@
+package com.practica2sd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2SdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practica2SdApplication.class, args);
+    }
+
+}
