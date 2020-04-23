f039a48ec6b5d87744a4d408497286c92f81dadb
JesusBorrero <shyfu98@gmail.com>
2020-04-23 17:24:09 +0200

    Busqueda simple de vuelos

--- Practica2SD/src/main/resources/static/assets/js/vuelos.js
+++ Practica2SD/src/main/resources/static/assets/js/vuelos.js
@@ -1,5 +1,7 @@
 var url = "http://localhost:8080/";
 
+var mapaAeropuertos = new Map();
+
 $(document).ready(function () {
     var opcionesAeropuertos = [];
 
@@ -11,6 +13,7 @@ $(document).ready(function () {
         type: "GET"
     }).done(function (aeropuertos) {
         $.each(aeropuertos, function (index, aeropuerto) {
+            mapaAeropuertos.set(aeropuerto.codigo, aeropuerto.id);
             opcionesAeropuertos.push(aeropuerto.codigo);
         });
 
@@ -23,3 +26,40 @@ $(document).ready(function () {
         });
     })
 });
+
+function busquedaVuelos() {
+
+    $("#resultadoVuelos").html("");
+
+    var aeropuertoOrigen = document.getElementById("aeropuertoOrigen").value;
+    var aeropuertoDestino = document.getElementById("aeropuertoDestino").value;
+    var diaSalida = document.getElementById("diaSalida").value;
+
+    if(aeropuertoOrigen === "" || aeropuertoDestino === "" || diaSalida === ""){
+        $("#resultadoVuelos").append("<div class='sin-resultados'>No hay resultados para esta búsqueda</div>");
+        return;
+    }
+
+    if(mapaAeropuertos.get(aeropuertoDestino) === undefined || mapaAeropuertos.get(aeropuertoOrigen) === undefined){
+        $("#resultadoVuelos").append("<div class='sin-resultados'>No hay resultados para esta búsqueda</div>");
+        return;
+    }
+
+    $.ajax({
+        url: url + "vuelo/find?idOrigen=" + mapaAeropuertos.get(aeropuertoOrigen) + "&idDestino=" +
+            mapaAeropuertos.get(aeropuertoDestino) + "&diaVuelo=" + diaSalida,
+        type: "GET"
+    }).done(function (vuelos) {
+        if(vuelos.length === 0){
+            $("#resultadoVuelos").append("<div class='sin-resultados'>No hay resultados para esta búsqueda</div>");
+        } else {
+            $.each(vuelos, function (index, vuelo) {
+                $("#resultadoVuelos").append("<div><p>Código: " + vuelo.codigoVuelo + "</p><p>Precio: " + vuelo.precio + "</p><p>Compañía: " + vuelo.compania.nombre + "</p></div>");
+            });
+        }
+    });
+
+    $("#aeropuertoOrigen").val("");
+    $("#aeropuertoDestino").val("");
+    $("#diaSalida").val("");
+}
