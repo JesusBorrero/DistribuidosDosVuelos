67ed4bcc4736761bdbdfb77a5e682061e80a490f
Alejandro <alexibro2@gmail.com>
2020-04-25 02:28:44 +0200

    Comentarios en el JS

--- Practica2SD/src/main/resources/static/assets/js/vuelos.js
+++ Practica2SD/src/main/resources/static/assets/js/vuelos.js
@@ -4,13 +4,16 @@ var mapaAeropuertos = new Map();
 var seleccionVuelosPrecio = new Map();
 var seleccionVuelosCompania = new Map();
 
+// Obtener todos los aeropuertos
+// - Se genera un mapa -> clave: código del aeropuerto - valor: id del aeropuerto
+// - Autocompleta la búsqueda de aeropuerto (salida y llegada)
 $(document).ready(function () {
     var opcionesAeropuertos = [];
 
     $.ajax({
         url: url + "aeropuerto/",
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
+            'Content-Type': 'application/json'
         },
         type: "GET"
     }).done(function (aeropuertos) {
@@ -31,6 +34,7 @@ $(document).ready(function () {
 
 function busquedaVuelos() {
 
+    // Inicialización de los componentes HTML
     $("#resultadoVuelos").html("");
     $("#resultadoVuelosIda").html("<h3>Vuelos de ida</h3>");
     $("#resultadoVuelosVuelta").html("<h3>Vuelos de vuelta</h3>");
@@ -40,6 +44,7 @@ function busquedaVuelos() {
     var aeropuertoDestino = document.getElementById("aeropuertoDestino").value;
     var diaSalida = document.getElementById("diaSalida").value;
 
+    // Validaciones previas a realizar la búsqueda
     if(aeropuertoOrigen === "" || aeropuertoDestino === "" || diaSalida === ""){
         $("#resultadoVuelos").append("<div class='sin-resultados'>No hay resultados para esta búsqueda</div>");
         return;
@@ -50,14 +55,18 @@ function busquedaVuelos() {
         return;
     }
 
+
+    // Petición para obtener todos los vuelos en función del origen, destino y dia de salida
     $.ajax({
         url: url + "vuelo/find?idOrigen=" + mapaAeropuertos.get(aeropuertoOrigen) + "&idDestino=" +
             mapaAeropuertos.get(aeropuertoDestino) + "&diaVuelo=" + diaSalida,
         type: "GET"
     }).done(function (vuelos) {
+        // No hay vuelos para los parámetros dados
         if(vuelos.length === 0){
             $("#resultadoVuelos").append("<div class='sin-resultados'>No hay resultados para esta búsqueda</div>");
         } else {
+            // Hay vuelos y se ha seleccionado ida-vuelta
             if(!$("#ida-vuelta-check").prop("checked")) {
                 $.each(vuelos, function (index, vuelo) {
                     $("#resultadoVuelos").append("<div class='card card-info-vuelo'><div class='card-body'><h2>Vuelo " + vuelo.codigoVuelo + "</h2>" +
@@ -65,6 +74,7 @@ function busquedaVuelos() {
                         + vuelo.compania.nombre + "</span></p></div></div>");
                 });
             } else {
+                // Hay vuelos y no se ha seleccionado ida-vuelta
                 $.each(vuelos, function (index, vuelo) {
                     $("#resultadoVuelosIda").append("<div class='card card-info-vuelo vuelo-ida-vuelta' onclick='seleccionVuelo(\"ida\", "+ vuelo.precio +", \"" + vuelo.compania.codigo + "\")'>" +
                         "<div class='card-body'><h3>Vuelo " + vuelo.codigoVuelo + "</h3>" +
@@ -75,23 +85,28 @@ function busquedaVuelos() {
         }
     });
 
+    // Si se ha seleccionado ida-vuelta, obtener todos los vuelos destino-origen en el día de retorno seleccionado
     if($("#ida-vuelta-check").prop("checked")) {
         $("#div-resultados-ida-vuelta").show();
         var diaRetorno = document.getElementById("diaRetorno").value;
 
+        // Validaciones previas a realizar la búsqueda
         if(diaRetorno === ""){
             $("#resultadoVuelosVuelta").append("<div class='sin-resultados'>No hay resultados para esta búsqueda</div>");
             return;
         }
 
+        // Petición para obtener todos los vuelos en función del destino, origen y dia de retorno
         $.ajax({
             url: url + "vuelo/find?idOrigen=" + mapaAeropuertos.get(aeropuertoDestino) + "&idDestino=" +
                 mapaAeropuertos.get(aeropuertoOrigen) + "&diaVuelo=" + diaRetorno,
             type: "GET"
         }).done(function (vuelos) {
+            // No hay vuelos para los parámetros dados
             if(vuelos.length === 0){
                 $("#resultadoVuelosVuelta").append("<div class='sin-resultados'>No hay resultados para esta búsqueda</div>");
             } else {
+                // Hay vuelos para los parámetros dados
                 $.each(vuelos, function (index, vuelo) {
                     $("#resultadoVuelosVuelta").append("<div class='card card-info-vuelo vuelo-ida-vuelta' onclick='seleccionVuelo(\"vuelta\", "+ vuelo.precio +", \""+ vuelo.compania.codigo + "\")'>" +
                         "<div class='card-body'><h2>Vuelo " + vuelo.codigoVuelo + "</h2>" +
@@ -102,22 +117,27 @@ function busquedaVuelos() {
         });
     }
 
+    // Limpiar los campos del formulario
     $("#aeropuertoOrigen").val("");
     $("#aeropuertoDestino").val("");
     $("#diaSalida").val("");
     $("#diaRetorno").val("");
 }
 
+// Mostrar la ficha de la compañia
 function fichaCompania(companiaId){
+    // Inicialización
     $("#dialogoCompania").html("");
 
+    // Obtener la información de una compañia
     $.ajax({
         url: url + "compania/" + companiaId,
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
+            'Content-Type': 'application/json'
         },
         type: "GET"
     }).done(function (compania) {
+        // Parámetros del diálogo que contiene la información de la compañia
         $( "#dialogoCompania" ).dialog({
             autoOpen: false,
             hide: "puff",
@@ -133,6 +153,7 @@ function fichaCompania(companiaId){
     })
 }
 
+// Si se selecciona ida-vuelta, mostrar el campo para seleccionar el día de retorno
 function checkIdaVuelta(){
     if($("#ida-vuelta-check").prop("checked")){
         $("#divDiaRetorno").show();
@@ -141,13 +162,17 @@ function checkIdaVuelta(){
     }
 }
 
+// Seleccionar vuelos y calcular precio total
 function seleccionVuelo(tipo, precioVuelo, companiaVuelo){
     seleccionVuelosPrecio.set(tipo, precioVuelo);
     seleccionVuelosCompania.set(tipo, companiaVuelo);
 
+    // Si se ha seleccionado vuelo de ida y vuelo de vuelta, calcular el precio total
     if(seleccionVuelosPrecio.get("ida") !== undefined && seleccionVuelosPrecio.get("vuelta") !== undefined){
 
         var precioTotal = seleccionVuelosPrecio.get("ida") + seleccionVuelosPrecio.get("vuelta");
+
+        // Si ambos vuelos pertenecen a la misma compañia, aplicar descuento
         if(seleccionVuelosCompania.get("ida") === seleccionVuelosCompania.get("vuelta")){
             precioTotal = precioTotal - precioTotal * 0.2;
             $("#precioTotal").append("<p>El precio total (reducido en 20%) es de: " + precioTotal + "</p>")
